039287ef58ad4477eda9a051939e836304fdb33d
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-02-07 13:14:31 +0530

    Undefined and Not defined in Javascript

---
+++ JS/Undefined_vs_not-defined/undefined_vs_not_defined.js
@@ -0,0 +1,19 @@
+// Undefined and Not Defined in Javascript
+
+
+// 1
+console.log(a); // Undefined
+var a="Tansen";
+console.log(a); // Tansen
+
+console.log(b); // Error : Not defined (b not declared)
+
+
+// 2
+
+console.log(a); // Undefined
+if(a===undefined){
+    console.log("Not defined"); // Not Defined 
+}
+var a = "Tansen"
+console.log(a); // Tansen
