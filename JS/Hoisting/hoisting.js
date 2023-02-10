3a550647bf13a2a3fc188ac6f7d0f9f80fd4c840
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-02-10 13:26:39 +0530

    Hoisting in javascript

---
+++ JS/Hoisting/hoisting.js
@@ -0,0 +1,20 @@
+// var a=5;
+
+// function getName(){
+//     console.log("Tansen Keshri");
+// }
+
+// getName();
+// console.log(a);
+
+test();
+getUser();
+console.log(a);
+
+function getName() {
+    console.log("Tansen Keshri")
+}
+
+const test = () => {
+    console.log("ProffessorLOGAN")
+}
